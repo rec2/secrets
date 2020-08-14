ce34363ad242edd9500dfc26f92eb3a01befb0f1
rec2 <ztengyang@gmail.com>
2020-08-14 14:11:16 -0500

    Enable user and password authentication

--- app.js
+++ app.js
@@ -1,1 +1,78 @@
 //jshint esversion:6
+const express = require("express");
+const bodyParse = require("body-parser");
+const ejs = require("ejs");
+const app = express();
+const mongoose = require("mongoose");
+
+app.use(express.static("public"));
+app.set("view engine" , "ejs");
+app.use(bodyParse.urlencoded({extended: true}));
+
+
+mongoose.connect("mongodb://localhost:27017/userDB", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+
+// Schema and model
+const userSchema = {
+    email: String,
+    password: String
+}
+
+const User = mongoose.model("User", userSchema);
+
+
+
+
+app.route("/") 
+    .get(function(req,res) {
+        res.render("home");
+});
+
+
+app.route("/login") 
+    .get(function(req,res) {
+        res.render("login");
+    })
+    .post(function(req,res){
+        const username = req.body.username;
+        const password = req.body.password;
+        // match user name with db, validate db password
+        User.findOne({email : username}, function(err,foundUser){
+            if(err) {
+                console.log(err);
+            } else {
+                if(foundUser) {
+                    if(foundUser.password === password);
+                    res.render("secrets")
+                }
+            }
+        });
+    });
+
+app.route("/register") 
+    .get(function(req,res) {
+        res.render("register");
+    })
+
+    .post(function(req,res) {
+        const newUser = new User({
+            email : req.body.username,
+            password : req.body.password
+        });
+        newUser.save(function(err){
+            if(!err) {
+                res.render("secrets");
+            } else {
+                res.render(err);
+            }
+        });
+
+    })
+
+app.listen(3000, function() {
+    console.log("Server up and running on port 3000");
+})
